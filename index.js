6ac71f279ed2d8e1a9ab99074b37353f99c959e8
toxqz <msaied333999@gmail.com>
2025-05-29 00:11:29 +0300

    Update index.js
    add basic server code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Marcel Mirror Bot is running.");
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Marcel Mirror Bot is alive!');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
